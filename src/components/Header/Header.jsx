93266f2f2f624baf4a65781f4297017a66a82ce0
Romiluna <romiluna2004@hotmail.com>
2021-11-06 00:39:54 -0300

    desafío entregable 2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Header = () => {
+    return(
+
+         <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Portefolio</a>
+            <a href="#">Contact</a>
+            <div class="animation start-home"></div>
+        </nav>
+        
+        
+    )
+}
+export default Header
