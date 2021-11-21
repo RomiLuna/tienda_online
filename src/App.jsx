71eb100b27a49dddc36cca34aa1d9d873a1c2677
Romiluna <romiluna2004@hotmail.com>
2021-11-21 17:16:55 -0300

    actualización de desafío 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { Fragment } from 'react';
+import { Fragment , useState } from 'react';
 import Header from './components/Header/Header.jsx';
 import Login from './components/Login/Login'
 import Input from './components/Input/Input.jsx';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 
-const App = () => {
+function App  ()  {
+    const [number, SetNumber] = useState(0)
+    
+     const incrementar = () => {
+        SetNumber(number + 1)
+    }
+
+    const decrementar = () => {
+        SetNumber(number - 1)
+    }
+
     return(
         <Fragment>
         <Header/>
         <ItemListContainer  greeting="Bienvenidos"/>
+        <h1>{`El número es ${number}`}</h1>
+        <button onClick={incrementar}>+</button>
+        <button onClick={decrementar}>-</button>
+
+
        {/* <Login/>  */}
 {/*<div>
     <Input styleClass="dark1" queTipoSoy="number"/>
